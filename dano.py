23539da053c78512b8c5c955a600b9dc62193402
Val Hovey <khoveyc@gmail.com>
2025-11-29 19:32:01 -0700

    Adding figures side-by-side

--- dano.py
+++ dano.py
@@ -28,16 +28,6 @@ for (angle, left, up) in directions:
     r_mask = (np.abs(x_rot + support_offset) < support_width/2) & centered & (left < support_width/2) & (up < 0)
     pupil[l_mask | r_mask] = 0.0
 
-plt.figure(figsize=(6,6))
-plt.imshow(pupil, origin='lower', cmap='gray',
-           extent=[-aperture_radius.value, aperture_radius.value,
-                   -aperture_radius.value, aperture_radius.value])
-plt.colorbar(label='Amplitude')
-plt.title("Danostar Spider")
-plt.xlabel("x [m]")
-plt.ylabel("y [m]")
-plt.show()
-
 pixelscale = 2*aperture_radius/(npix * u.pixel)
 array_pupil = ArrayOpticalElement(
         transmission=pupil,
@@ -48,7 +38,7 @@ osys = OpticalSystem(pupil_diameter=2*aperture_radius)
 osys.add_pupil(array_pupil)
 
 detector_pixelscale = (1.41 / 3) * u.arcsec / u.pixel  # arcsec per pixel
-osys.add_detector(pixelscale=detector_pixelscale, fov_pixels=500)
+osys.add_detector(pixelscale=detector_pixelscale, fov_pixels=400)
 
 colors = [
     np.arange(620e-9, 700e-9, 10e-9),
@@ -84,9 +74,35 @@ norm = (log_psf_channels - min_val) / (max_val - min_val)
 # Rearrange to (H, W, 3) for RGB
 rgb_image = np.moveaxis(norm, 0, -1)
 
-# Display
-plt.figure(figsize=(6,6), facecolor='black')
-plt.imshow(rgb_image, origin='lower')
-plt.axis('off')
-plt.tight_layout(pad=0)
-plt.show()
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+# ------------------------
+# Left pane: Aperture
+# ------------------------
+ax = axes[0]
+im0 = ax.imshow(
+    pupil,
+    origin='lower',
+    cmap='gray',
+    extent=[-aperture_radius.value, aperture_radius.value,
+            -aperture_radius.value, aperture_radius.value]
+)
+fig.colorbar(im0, ax=ax, label='Amplitude')
+ax.set_title("Danostar Spider")
+ax.set_xlabel("x [m]")
+ax.set_ylabel("y [m]")
+
+# ------------------------
+# Right pane: PSF (RGB)
+# ------------------------
+ax = axes[1]
+ax.imshow(rgb_image, origin='lower')
+ax.set_title("RGB PSF")
+ax.axis('off')
+
+plt.tight_layout()
+
+# Save instead of show
+fig.savefig("aperture_and_psf.png", dpi=200, bbox_inches='tight')
+
+plt.close(fig)
