3d2d0c292f44479f80f8efdeec6250e3d7e7b68f
Val Hovey <khoveyc@gmail.com>
2025-11-30 12:30:42 -0700

    Organized logic into module

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
-def main():
-    print("Hello from star-simulations!")
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+import simulation as sim
+from typing import cast
 
+npix = 1024
+aperture_radius = cast(u.Quantity, 0.01 * u.m)
+secondary_radius = cast(u.Quantity, 0.003 * u.m)
+support_width = cast(u.Quantity, 0.0005 * u.m)
+support_offset_max = cast(u.Quantity, 0.0015 * u.m)
+frames = 200
+
+d_offset = support_offset_max / frames
+
+def gen_frame(i, support_offset):
+    pupil = sim.Pupil(aperture_radius, npix)
+    pupil.add_secondary(secondary_radius)
+    pupil.add_offset_supports(support_width, support_offset)
+
+    osys = pupil.to_optical_system(0.7)
+
+    rgb_image = sim.simulate_psf(sim.rgb, osys)
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    ax = axes[0]
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(
+        pupil.pupil,
+        origin='lower',
+        cmap='gray',
+        extent=[-aperture_radius.value, aperture_radius.value,
+                -aperture_radius.value, aperture_radius.value]
+    )
+    ax.set_title("Spider Shape")
+
+    ax = axes[1]
+    ax.imshow(rgb_image, origin='lower')
+    ax.set_title("RGB PSF")
+    ax.axis('off')
+
+    plt.tight_layout()
+
+    fig.savefig(f"frame_{i}.png", dpi=300, bbox_inches='tight')
+
+    plt.close(fig)
 
 if __name__ == "__main__":
-    main()
+    # Single shot
+    gen_frame(0, 0)
+
+    # Animation
+    # for i, support_offset in enumerate([0 + n * d_offset for n in range(frames)]):
+    #     gen_frame(i, support_offset)
---
+++ simulation.py
@@ -0,0 +1,79 @@
+from typing import Any
+import numpy as np
+import astropy.units as u
+from dataclasses import dataclass
+from numpy.typing import NDArray
+from poppy import ArrayOpticalElement, OpticalSystem
+
+@dataclass
+class Pupil:
+    radius: u.Quantity
+    pixels: int
+    xx: NDArray[Any]
+    yy: NDArray[Any]
+    pupil: NDArray[Any]
+
+    def __init__(self, radius: u.Quantity, pixels: int):
+        x = np.linspace(-radius.value, radius.value, pixels) * u.m
+        self.radius = radius
+        self.pixels = pixels
+        self.xx, self.yy = np.meshgrid(x, x)
+        self.pupil = ((self.xx**2 + self.yy**2) <= radius**2).astype(float)
+
+    def add_secondary(self, radius):
+        self.pupil *= ((self.xx**2 + self.yy**2) >= radius**2).astype(float)
+
+    def add_offset_supports(self, width: u.Quantity, offset: u.Quantity):
+        directions = [(0, self.xx, self.yy), (np.pi/2, self.yy, self.xx)]
+        for (angle, left, up) in directions:
+            x_rot = self.xx * np.cos(angle) + self.yy * np.sin(angle)
+            centered = ((self.xx**2 + self.yy**2) <= self.radius**2)
+            l_mask = (np.abs(x_rot - offset) < offset/2) & centered & (left > -width/2) & (up > 0)
+            r_mask = (np.abs(x_rot + offset) < offset/2) & centered & (left < width/2) & (up < 0)
+            self.pupil[l_mask | r_mask] = 0.0
+
+    def to_optical_system(self, arcsec_per_pixel, fov_pixels=800):
+        pixelscale = 2*self.radius/(self.pixels * u.pixel)
+        array_pupil = ArrayOpticalElement(
+                transmission=self.pupil,
+                pixelscale=pixelscale,
+        )
+
+        osys = OpticalSystem(pupil_diameter=2*self.radius)
+        osys.add_pupil(array_pupil)
+
+        detector_pixelscale = arcsec_per_pixel * u.arcsec / u.pixel
+        osys.add_detector(pixelscale=detector_pixelscale, fov_pixels=fov_pixels)
+
+        return osys
+
+rgb = [
+    np.arange(620e-9, 700e-9, 10e-9),
+    np.arange(480*1e-9, 570*1e-9, 10*1e-9),
+    np.arange(420*1e-9, 510*1e-9, 10*1e-9),
+]
+
+def simulate_psf(colors, osys):
+    eps = 1e-12
+
+    log_psf_channels = []
+
+    for wavelengths in colors:
+        source = {
+                "wavelengths": wavelengths,
+                "weights": np.ones_like(wavelengths)
+        }
+
+        psf = osys.calc_psf(source=source)
+        psf_data = psf[0].data
+
+        log_psf = np.log10(psf_data + eps)
+        log_psf_channels.append(log_psf)
+
+    log_psf_channels = np.array(log_psf_channels)
+
+    min_val = log_psf_channels.min()
+    max_val = log_psf_channels.max()
+    norm = (log_psf_channels - min_val) / (max_val - min_val)
+
+    return np.moveaxis(norm, 0, -1)
